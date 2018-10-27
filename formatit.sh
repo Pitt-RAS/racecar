e858a1e1799b7942bf8fb69f1772fc1c36c6a9fd
Andrew Lobos <andrew@lobos.me>
2018-10-27 14:36:49 -0400

    Less aggressive autoformat (#51)

--- formatit.sh
+++ formatit.sh
@@ -6,6 +6,6 @@ pushd $(dirname $BASH_SOURCE) > /dev/null
 find src -iname "*.h" -o -iname "*.cpp" | xargs uncrustify --replace --no-backup -c uncrustify.cfg
 
 # Python formatting
-find src -iname "*.py" | xargs autopep8 --in-place --max-line-length 120 --aggressive --aggressive
+find src -iname "*.py" | xargs autopep8 --in-place --max-line-length 120
 
 popd > /dev/null
