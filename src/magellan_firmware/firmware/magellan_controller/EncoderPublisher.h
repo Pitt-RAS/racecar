2ed38f78ca5dd3ba7a0725a16608973e9711d11a
nls5260 <nls5260@gmail.com>
2019-07-07 15:53:58 -0400

    updated fsae encoder publisher files

--- src/magellan_firmware/firmware/magellan_controller/EncoderPublisher.h
+++ src/magellan_firmware/firmware/magellan_controller/EncoderPublisher.h
@@ -20,15 +20,15 @@ private:
     ros::NodeHandle& nh_;
     geometry_msgs::TwistWithCovarianceStamped twist_msg_;
     ros::Publisher velocity_publisher_;
-    magellan_core::EncoderCount encoder_count_msg_;
+    fsae_core::EncoderCount encoder_count_msg_;
     ros::Publisher encoder_count_publisher_;
     Rate update_rate_;
     Rate update_rate_debug_;
     long int last_count_A;
     long int last_count_B;
     long int last_count_C;
-    double compute_distance(int steps);
-    float compute_velocity(int delta);
+    double compute_distance(double steps);
+    double compute_velocity(double delta);
 };
 
 #endif
