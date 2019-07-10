38fff36ff865484f6e2e03893b1d9ce91d7c7a95
noah <noahlaf@gmail.com>
2019-07-10 01:19:17 -0400

    Fix typos

--- src/magellan_firmware/firmware/magellan_controller/EncoderPublisher.h
+++ src/magellan_firmware/firmware/magellan_controller/EncoderPublisher.h
@@ -20,8 +20,8 @@ private:
     ros::NodeHandle& nh_;
     geometry_msgs::TwistWithCovarianceStamped twist_msg_;
     ros::Publisher velocity_publisher_;
-    magellan_core::EncoderCount encoder_count_msg_;
-    ros::Publisher encoder_count_publisher_;
+    magellan_core::EncoderCount encoder_total_msg_;
+    ros::Publisher encoder_total_publisher_;
     Rate update_rate_;
     Rate update_rate_debug_;
     double compute_distance(double steps);
