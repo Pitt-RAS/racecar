1168f4eea5efa64c5ff8f29da76cccdc19dc6c7b
noah <noahlaf@gmail.com>
2019-07-20 18:54:13 -0400

    odom subscriber and callback temp

--- src/magellan_motion/src/new_path_follower/force_cmd.cpp
+++ src/magellan_motion/src/new_path_follower/force_cmd.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <std_msgs/Float64.h>
 #include <std_msgs/String.h>
+#include <nav_msgs/Odometry.h>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2/utils.h>
@@ -9,9 +10,13 @@ std_msgs::Float64 cmd_vel;
 std_msgs::Float64 cmd_turn;
 std_msgs::String debug_string;
 
+float current_x = 0;
+float current_y = 0;
+float current_theta = 0;
+
 void printDebug(std::string ting, ros::Publisher debug_publisher);
 
-float nextPoint[4][2] = {{0,0},{2,0},{2,2},{0,2}}; // this is garbage change later | float[1*n] = x, float[2*n] = y;
+float nextPoint[4][2] = {{0,0},{2,0},{2,2},{0,2}}; // this is garbage change later 
 
 int main(int argc, char** argv){
 	// Setup publishers and rate 
@@ -23,6 +28,7 @@ int main(int argc, char** argv){
 	ros::Publisher velocity_publisher = nh.advertise<std_msgs::Float64>("/platform/cmd_velocity", 10);
 	ros::Publisher turning_radius_publisher = nh.advertise<std_msgs::Float64>("/platform/cmd_turning_radius", 10);
 	ros::Publisher debug_publisher = nh.advertise<std_msgs::String>("/my_debug", 10);
+	ros::Subscriber odom_ekf_subscriber = nh.subscribe<nav_msgs::Odometry>("/odometry/filtered", 10, odom_ekf_callback);
 	tf2_ros::TransformBroadcaster br;
 
 	double rate_hz = 100;
@@ -61,4 +67,10 @@ void updateNextPoint(){
 void printDebug(std::string ting, ros::Publisher debug_publisher){
 	debug_string.data = ting;
 	debug_publisher.publish(debug_string);
+}
+
+void odom_ekf_callback(nav_msgs::Odometry msg){
+	current_x = msg.pose.x;
+	current_y = msg.pose.y;
+	current_theta = something;
 }
