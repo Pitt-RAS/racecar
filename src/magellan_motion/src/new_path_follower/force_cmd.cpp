3561ea495fc99b39e0958f287fb9407329d9dac3
noah <noahlaf@gmail.com>
2019-07-20 18:23:01 -0400

    added debug

--- src/magellan_motion/src/new_path_follower/force_cmd.cpp
+++ src/magellan_motion/src/new_path_follower/force_cmd.cpp
@@ -1,8 +1,17 @@
 #include <ros/ros.h>
 #include <std_msgs/Float64.h>
+#include <std_msgs/String.h>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2/utils.h>
+
+std_msgs::Float64 cmd_vel;
+std_msgs::Float64 cmd_turn;
+std_msgs::String debug_string;
+
+void printDebug(std::string ting, ros::Publisher debug_publisher);
+
+float nextPoint[4][2] = {{0,0},{2,0},{2,2},{0,2}}; // this is garbage change later | float[1*n] = x, float[2*n] = y;
 
 int main(int argc, char** argv){
 	// Setup publishers and rate 
@@ -13,14 +22,12 @@ int main(int argc, char** argv){
 
 	ros::Publisher velocity_publisher = nh.advertise<std_msgs::Float64>("/platform/cmd_velocity", 10);
 	ros::Publisher turning_radius_publisher = nh.advertise<std_msgs::Float64>("/platform/cmd_turning_radius", 10);
+	ros::Publisher debug_publisher = nh.advertise<std_msgs::String>("/my_debug", 10);
 	tf2_ros::TransformBroadcaster br;
 
 	double rate_hz = 100;
 	private_nh.getParam("rate", rate_hz);
 	ros::Rate rate(rate_hz);
-
-	std_msgs::Float64 cmd_vel;
-	std_msgs::Float64 cmd_turn;
 
 	// Setup transform listener
 	tf2_ros::Buffer tf_buffer;
@@ -36,9 +43,22 @@ int main(int argc, char** argv){
 	while(ros::ok()){
 		velocity_publisher.publish(cmd_vel);
 		turning_radius_publisher.publish(cmd_turn);
+		printDebug("Fuck", debug_publisher);
 
 		ros::spinOnce();
 		rate.sleep();
 	}
 }
 
+/*
+void updateNextPoint(){
+	if(){
+
+	}
+}
+*/
+
+void printDebug(std::string ting, ros::Publisher debug_publisher){
+	debug_string.data = ting;
+	debug_publisher.publish(debug_string);
+}
