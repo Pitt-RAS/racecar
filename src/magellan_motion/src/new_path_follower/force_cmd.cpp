f4a6702684a76799871a5873182e7b8331b2e458
noah <noahlaf@gmail.com>
2019-07-21 11:50:12 -0400

    make it go

--- src/magellan_motion/src/new_path_follower/force_cmd.cpp
+++ src/magellan_motion/src/new_path_follower/force_cmd.cpp
@@ -5,6 +5,8 @@
 #include <tf2_ros/transform_listener.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2/utils.h>
+#include <math.h>
+#include <tf/transform_datatypes.h>
 
 std_msgs::Float64 cmd_vel;
 std_msgs::Float64 cmd_turn;
@@ -12,11 +14,29 @@ std_msgs::String debug_string;
 
 float current_x = 0;
 float current_y = 0;
-float current_theta = 0;
+double current_theta = 0;
+nav_msgs::Odometry current_odometry;
+
+float goal_x = 0;
+float goal_y = 0;
+float goal_theta = 0;
+
+int pointTick = 0;
+
+float euclideanDistance = 0;
+float distanceTolerance = 0.2;
 
 void printDebug(std::string ting, ros::Publisher debug_publisher);
+void updateNextPoint(float tolerance);
+void updateTheta(float tolerance, float constant);
+void odom_ekf_callback(nav_msgs::Odometry::ConstPtr msg);
+void updateEuclideanDistance();
+void updateRobot(ros::Publisher velocity_publisher, ros::Publisher turning_radius_publisher);
+std::string floatToString (float number);
 
-float nextPoint[4][2] = {{0,0},{2,0},{2,2},{0,2}}; // this is garbage change later 
+
+
+float points[4][2] = {{0,0},{1,0},{1,1},{0,1}}; // this is garbage change later 
 
 int main(int argc, char** argv){
 	// Setup publishers and rate 
@@ -35,42 +55,84 @@ int main(int argc, char** argv){
 	private_nh.getParam("rate", rate_hz);
 	ros::Rate rate(rate_hz);
 
+	/*
 	// Setup transform listener
 	tf2_ros::Buffer tf_buffer;
 	tf2_ros::TransformListener tf_listener(tf_buffer);
-	// End setup
 
 	//waypoint is called fake_point
 
-	auto transform = tf_buffer.lookupTransform("base_link", "fake_point", ros::Time(0), ros::Duration(10.0));
+
+	ros::Time now = ros::Time::now();
+	tf_buffer.waitForTransform("base_link", "fake_point", now, ros::Duration(3.0));
+
+	auto transform = tf_buffer.lookupTransform("base_link", "fake_point", ros::Time::now(), ros::Duration(10.0));
 	br.sendTransform(transform);
-	cmd_vel.data = 1.0;
-	cmd_turn.data = 0.8;
+	*/
 	while(ros::ok()){
-		velocity_publisher.publish(cmd_vel);
-		turning_radius_publisher.publish(cmd_turn);
-		printDebug("Fuck", debug_publisher);
+		updateEuclideanDistance();
+		if(euclideanDistance > distanceTolerance){
+			updateRobot(velocity_publisher, turning_radius_publisher);
+		}
+
+		updateTheta(0.1, 6);	//TODO: determine constant so that smallest angle * constant = lowest cmd_turn value and visa versa
+		updateNextPoint(.2);
+		
+		printDebug(floatToString(current_y) + " <-y theta-> " + floatToString(current_theta), debug_publisher);
 
 		ros::spinOnce();
 		rate.sleep();
 	}
 }
 
-/*
-void updateNextPoint(){
-	if(){
+void updateRobot(ros::Publisher velocity_publisher, ros::Publisher turning_radius_publisher){
+	velocity_publisher.publish(cmd_vel);
+	turning_radius_publisher.publish(cmd_turn);
+}
 
+void updateNextPoint(float tolerance){
+	if(abs(current_x - points[pointTick][0]) < tolerance && abs(current_y - points[pointTick][1]) < tolerance){
+		pointTick++;
+		goal_x = points[pointTick][0];
+		goal_y = points[pointTick][1];
 	}
 }
-*/
+
+void updateTheta(float tolerance, float constant){
+	if(abs(goal_theta - current_theta) > tolerance);{
+	cmd_turn.data = constant * (goal_theta - current_theta);
+	}
+}
 
 void printDebug(std::string ting, ros::Publisher debug_publisher){
 	debug_string.data = ting;
 	debug_publisher.publish(debug_string);
 }
 
-void odom_ekf_callback(nav_msgs::Odometry msg){
-	current_x = msg.pose.x;
-	current_y = msg.pose.y;
-	current_theta = something;
+void odom_ekf_callback(nav_msgs::Odometry::ConstPtr msg){
+	//current_odometry = msg;
+
+	current_x = msg->pose.pose.position.x;
+	current_y = msg->pose.pose.position.y;
+
+	tf::Quaternion quaternion(
+								msg->pose.pose.orientation.x,
+								msg->pose.pose.orientation.y,
+								msg->pose.pose.orientation.z,
+								msg->pose.pose.orientation.w);
+	tf::Matrix3x3 matrix(quaternion);
+	double roll, pitch, yaw;
+	matrix.getRPY(roll, pitch, yaw);
+
+	current_theta = yaw;
+}
+
+std::string floatToString (float number){
+    std::ostringstream buff;
+    buff<<number;
+    return buff.str();   
+}
+
+void updateEuclideanDistance(){
+	euclideanDistance = sqrt(pow(current_x - goal_x, 2) + pow(current_y - goal_y, 2));
 }
