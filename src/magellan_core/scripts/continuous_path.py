ff8c08970714a5974041f594e9dcb6d928bea0a8
noah <noahlaf@gmail.com>
2019-08-06 13:13:45 -0400

    cleanup

--- src/magellan_core/scripts/continuous_path.py
+++ src/magellan_core/scripts/continuous_path.py
@@ -16,15 +16,13 @@ class ContinuousPathNode(object):
         self._tf_buffer = tf2_ros.Buffer()
         self._tf_listener = tf2_ros.TransformListener(self._tf_buffer)
 
-        #self._raw_path = rospy.get_param('~path')
         self._path = []
-        self._step_size = rospy.get_param('~step_size')
-        self._frame_id = rospy.get_param('~path_frame')
-        self._odom_frame_id = rospy.get_param('~odom_frame')
+        self._step_size = 0.1
+        self._frame_id = 'odom'
+        self._odom_frame_id = 'odom'
         self._publisher = rospy.Publisher('/path', Path, queue_size=5)
         self.raw_path_sub = rospy.Subscriber('/noah/raw_path', Point, self.raw_path_callback)
 
-    # so raw_path should be a topic where a geometry_msgs Point gets published
     def raw_path_callback(self, data):
         now = rospy.Time.now()
         try:
@@ -33,7 +31,6 @@ class ContinuousPathNode(object):
             print(e)
             rospy.logwarn("fake_path: Couldn't lookup transform {} -> {}".format(self._frame_id, self._odom_frame_id))
             return
-        #self._path = []
         header = Header(frame_id=self._frame_id, stamp=now)
         waypoint = PointStamped()
         waypoint.header = header
@@ -41,14 +38,13 @@ class ContinuousPathNode(object):
         waypoint.point.y = data.y
 
         waypoint = do_transform_point(waypoint, transform)
-        rospy.loginfo("fake_path: appending coordinates | _path length = " + str(len(self._path)))
         self._path.append([waypoint.point.x, waypoint.point.y])
         rospy.loginfo("fake_path: Path retransform")
         self.publish_path()
 
     def publish_path(self):
         if len(self._path) < 2:
-            rospy.logwarn("fake_path: Path is empty" + str(len(self._path)))
+            rospy.logwarn("fake_path: Path is empty")
             return
         path = list(self._path)
         path_msg = Path()
@@ -77,21 +73,16 @@ if __name__ == '__main__':
     rospy.init_node('continuous_path')
 
     node = ContinuousPathNode()
-    rate = rospy.Rate(1)
-    retransform_period = rospy.Duration(5)
-    last_retransform = rospy.Time(0)
     debug_raw_path_pub = rospy.Publisher('/noah/raw_path', Point, queue_size=5)
 
     debug_point = Point()
     debug_tick = 0
     rospy.sleep(2)
-    
-    #initialize first point on path to origin
+
+    # initialize first point on path to origin
     debug_point.x = 0
     debug_point.y = 0
     debug_point.z = 0
     debug_raw_path_pub.publish(debug_point)
     while not rospy.is_shutdown():
-        rospy.loginfo("enter spin")
         rospy.spin()
-        rospy.loginfo("exit spin")
