ad4bb85653671013489e9b5ef6b1171bf065dda2
camkisailus <47364146+camkisailus@users.noreply.github.com>
2019-12-14 12:43:05 -0500

    ROSnode for line detection (#16)
    * Updated line_detector.py to work with realsense stream
    
    * Lint
    
    * Lint 2x
    
    * ROS implementation, added TODOs and relevant comments
    
    * ROS implementation
    
    * First step of ROS Implementation
    
    * Commented for fixes needed
    
    * ROS Implementation: Subscribes from the real sense ros wrapper node and published processed image to a node
    
    * Updated comments and TODOs and unnecessary code deletions
    
    * Adding private variables and some cleanup
    
    * Lint Cleanup and CvBridge try catch addition
    
    * Modified private variables
    
    * Added point array publisher
    
    * Lint + Update private variables in Lines class (Verify if working)
    
    * Updated points Msg, updated CMakeLists.txt, updated line_detector.py and it works
    
    - Message now contains X and Y points seperately in array format
    - De linted and now works with the real sense camera
    
    * added launch file
    
    * inherit from object
    
    * minor fixes
    
    * Fixed
    
    * linter
    
    * lint
    
    * params added and implemented
    
    * lint
    
    * minor fixes
    
    * quickfix
    
    * Lowered CPU usage
    
    * Linters exist for a reason
    
    * Lint 2x
    
    * remove init.sh
    
    * PR comments
    
    * catch all
    
    * PointArray msg
    
    * linter no like
    
    * Fixed Point msg
    
    * cleaning passive and adding error print to line detector
    
    * Fixed launch

--- src/magellan_vision/scripts/line_detector.py
+++ src/magellan_vision/scripts/line_detector.py
@@ -1,211 +1,170 @@
+#!/usr/bin/env python
+
+import sys
+import math
+import threading
+
+
 import cv2
 import numpy as np
-import time
-import math
-from statistics import mean
-# import matplotlib.pyplot as plt
-
-ddepth = cv2.CV_16S
-kernel_size = 3
-skipped_frames = 0
-num_of_frames = 0
 
 
-def skeletize(img, size, skel):
-    element = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
-    done = False
-    while(not done):
-        eroded = cv2.erode(img, element)
-        temp = cv2.dilate(eroded, element)
-        temp = cv2.subtract(img, temp)
-        skel = cv2.bitwise_or(skel, temp)
-        img = eroded.copy()
-        zeros = size - cv2.countNonZero(img)
-        if zeros == size:
-            done = True
-    return skel
+# ROS imports
+import rospy
+import std_msgs.msg
+from sensor_msgs.msg import Image  # ROS Image Message
+from geometry_msgs.msg import Point
+from magellan_core.msg import PointArray
+from cv_bridge import CvBridge, CvBridgeError  # Converts b/w OpenCV Image and ROS Image Message
+
+point_arr = PointArray()
 
 
-def draw_lines(img, lines, color=[0, 255, 0], thickness=3):
-    if lines is None:
-        return
-    img = np.copy(img)
-    line_img = np.zeros(
-        (
-            img.shape[0],
-            img.shape[1],
-            3,
-        ),
-        dtype=np.uint8,
-    )
-    for line in lines:
-        for x1, y1, x2, y2 in line:
-            cv2.line(line_img, (x1, y1), (x2, y2), color, thickness)
+class PubSubNode(object):
+    def __init__(self):
+        # TODO FIX THIS
+        try:
+            # Global Constant
+            self._VERBOSE = rospy.get_param("verbose")
+        except KeyError as e:
+            rospy.logerr("LineDetector: error in looking up param. {}".format(e))
+            raise
 
-    img = cv2.addWeighted(img, 0.8, line_img, 1.0, 0.0)
-    return img
+        self._lock = threading.RLock()
 
+        '''Initialize ros publisher, ros subscriber'''
+        self._lines_object = Lines()
+        # topic where we publish
+        self._image_pub = rospy.Publisher("/perception/color/image_processed", Image, queue_size=5)
+        self._point_arr_pub = rospy.Publisher('/perception/detected_points', PointArray, queue_size=5)
+        self._bridge = CvBridge()
 
-def best_fit(x_points, y_points):
+        # subscribed Topic
+        self._subscriber = rospy.Subscriber("/camera/color/image_raw", Image, self._callback, queue_size=1)
+        self._image = None
 
-    m = (((mean(x_points)*mean(y_points)) - mean(x_points*y_points)) /
-         ((mean(x_points)*mean(x_points))-mean(x_points*x_points)))
+    def run_detects(self):
+        with self._lock:
+            header = std_msgs.msg.Header()
+            header.stamp = rospy.Time.now()
+            point_arr.clear()
+            point_arr.header = header
+            point_arr.points = []
+            # Line detection
+            if self._image is not None:
+                image_np = self._lines_object.detect(self._image)
 
-    b = mean(y_points) - m*mean(x_points)
+                # conversion back to Image
+                try:
+                    ros_msg = self._bridge.cv2_to_imgmsg(image_np)
+                except CvBridgeError:
+                    rospy.logerr('Could not convert image')
+                    return
 
-    return m, b
+                # Publish Processed Image amnd Points
+                self._image_pub.publish(ros_msg)
 
+    '''Callback function of subscribed topic.
+    Here images get converted and features detected and published'''
 
-def process_lines(linesP, frame):
-    i = 0
-    left_line_x = []
-    left_line_y = []
-    right_line_x = []
-    right_line_y = []
-
-    crop = frame
-
-    for line in linesP:
-        for x1, y1, x2, y2 in line:
-            if i == 0:
-                i += 1
-                max_y = y2
-                min_y = y1
-            if y2 < min_y:
-                min_y = y1
-            if y1 < max_y:
-                max_y = y2
-            slope = (y2 - y1) / (x2 - x1)  # <-- Calculating the slope.
-        if math.fabs(slope) < 0.8:  # <-- Only consider extreme slope
-            continue
-        if x1 < frame.shape[1]/2 and x2 < frame.shape[1]/2:  # <-- If the slope is negative, left group.
-            left_line_x.extend([x1, x2])
-            left_line_y.extend([y1, y2])
-        else:  # <-- Otherwise, right group.
-            right_line_x.extend([x1, x2])
-            right_line_y.extend([y1, y2])
-    # TODO: Dynamically assign min and max y, using the iteration above???
-    min_y = (int)(frame.shape[0] * (3/5))
-    max_y = frame.shape[0]
-
-    # TODO: Instead of poly, maybe try cv2.rectangle to match the area covered by linesP more accurately
-    if len(left_line_x) != 0 and len(left_line_y) != 0 and len(right_line_y) != 0 and len(right_line_x) != 0:
-        poly_left = np.poly1d(np.polyfit(
-            left_line_y,
-            left_line_x,
-            deg=1
-        ))
-        left_x_start = int(poly_left(max_y))
-        left_x_end = int(poly_left(min_y))
-
-        poly_right = np.poly1d(np.polyfit(
-            right_line_y,
-            right_line_x,
-            deg=1
-        ))
-        right_x_start = int(poly_right(max_y))
-        right_x_end = int(poly_right(min_y))
-        # TODO: Filter out "bad" lines (i.e. if the two lines intersect)
-        crop = draw_lines(
-            frame,
-            [[
-                [left_x_start, max_y, left_x_end, min_y],
-                [right_x_start, max_y, right_x_end, min_y],
-            ]],
-            thickness=5,
-        )
-
-    cv2.imshow('Processed', crop)
-    return crop
+    def _callback(self, ros_data):
+        with self._lock:
+            try:
+                self._image = self._bridge.imgmsg_to_cv2(ros_data, "bgr8")
+            except CvBridgeError:
+                rospy.logerr('Could not convert image')
+                return
 
 
-video = cv2.VideoCapture("IGVC_Video.mp4")
-while True:
-    ret, frame = video.read()
-    tic = time.time()
-    height, width, channels = frame.shape
-    # print("height: {}, width: {}".format(height,width))
-    if not ret:
-        video = cv2.VideoCapture("IGVC_Video.mp4")
-        continue
-    num_of_frames += 1
-    # print(num_of_frames)
-    crop = frame[360:670, 0:1280]
-    gray = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
-    blurred = cv2.GaussianBlur(gray, (3, 3), 0)
-    ret, thresh4 = cv2.threshold(blurred, 200, 255, cv2.THRESH_TOZERO)
-    # cv2.imshow(cropped_image)
-    size = np.size(thresh4)
-    skel = np.zeros(thresh4.shape, np.uint8)
+''' Class Lines: Detects points in an Image that aligns into a line.
+    Outputs the image with detected points overlayed on it.'''
 
-    skeleton = skeletize(thresh4, size, skel)
 
-    dst = cv2.Laplacian(skeleton, ddepth, ksize=kernel_size)
-    abs_dst = cv2.convertScaleAbs(dst)
+class Lines(object):
 
-    linesP = cv2.HoughLinesP(abs_dst, rho=6, theta=np.pi / 60, threshold=50,
-                             lines=np.array([]), minLineLength=30, maxLineGap=10)
+    def __init__(self):
+        self._ddepth = cv2.CV_16S
+        try:
+            self._KERNEL_SIZE = rospy.get_param("kernel_size")
+            self._RHO = rospy.get_param("rho")
+            self._THETA = rospy.get_param("theta")
+            self._THRESHOLD = rospy.get_param("threshold")
+            self._MINLINELENGTH = rospy.get_param("min_line_length")
+            self._MAXLINEGAP = rospy.get_param("max_line_gap")
+        except KeyError as e:
+            rospy.logerr("LineDetector: error in looking up param. {}".format(e))
+            raise
 
-    # x_array_left = [[]]
-    # x_array_right = [[]]
-    # print(x_array.shape)
-    # y_array_left = [[]]
-    # y_array_right = [[]]
-    # print(y_array.shape)
+    def skeletize(self, img, size, skel):
+        element = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
+        while not rospy.is_shutdown():
+            eroded = cv2.erode(img, element)
+            temp = cv2.dilate(eroded, element)
+            temp = cv2.subtract(img, temp)
+            skel = cv2.bitwise_or(skel, temp)
+            img = eroded.copy()
+            zeros = size - cv2.countNonZero(img)
+            if zeros == size:
+                break
 
-    crop2 = crop
+        return skel
 
-    if linesP is not None and len(linesP) > 40:
-        # processed = process_lines(linesP,crop)
-        # print(len(linesP))
-        for line in linesP:
-            for x1, y1, x2, y2 in line:
-                slope = (y2 - y1) / (x2 - x1)
-                # <-- Calculating the slope.
-                if math.fabs(slope) < .5:
-                    # <-- Only consider extreme slope
-                    continue
-                if x1 < frame.shape[1]/2 and x2 < frame.shape[1]/2:
-                    # <-- If the slope is negative, left group
-                    # x_array_left.append([x1,x2])
-                    # y_array_left.append([y1,y2])
-                    cv2.line(crop2, (x1, y1), (x2, y2), (0, 0, 255), 3)
-                    # cv2.circle(crop, (x1, y1),(5), (0,0,255), 3)
-                    # cv2.circle(crop, (x2, y2),(5), (0,0,255), 3)
-                else:  # <-- Otherwise, right group.
-                    # x_array_right.append([x1,x2])
-                    # y_array_right.append([y1,y2])
-                    cv2.line(crop2, (x1, y1), (x2, y2), (0, 255, 0), 3)
-                    # cv2.circle(crop, (x1, y1),(5), (0,255,0), 3)
-                    # cv2.circle(crop, (x2, y2),(5), (0,255,0), 3)
-            circles = crop
-            lines = crop2
+    def detect(self, cv_image):
+        gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+        blurred = cv2.GaussianBlur(gray, (3, 3), 0)
+        ret, thresh4 = cv2.threshold(blurred, 200, 255, cv2.THRESH_TOZERO)
+        size = np.size(thresh4)
+        skel = np.zeros(thresh4.shape, np.uint8)
 
-    if linesP is None or len(linesP) < 40:
-        skipped_frames += 1
+        skeleton = self.skeletize(thresh4, size, skel)
 
-    cv2.imshow('Crop', crop)
-    # linesAndCircles = cv2.addWeighted(lines, 0.5, circles, 0.5,0)
-    # final = cv2.addWeighted(processed, 0.8, linesAndCircles, 0.5,0)
-    # cv2.imshow('Original', frame)
-    # cv2.imshow('Combo', final)
-    # print(skipped_frames)
+        dst = cv2.Laplacian(skeleton, self._ddepth, ksize=self._KERNEL_SIZE)
+        abs_dst = cv2.convertScaleAbs(dst)
 
-    # FIXME: what is this crap, please FIX ME
-    # plt.plot(np.unique(x_array_right),
-    #          np.poly1d(np.polyfit(x_array_right, y_array_right, 1))
-    #          (np.unique(x_array_right)))
+        linesP = cv2.HoughLinesP(abs_dst, rho=self._RHO, theta=self._THETA, threshold=self._THRESHOLD,
+                                 lines=np.array([]), minLineLength=self._MINLINELENGTH, maxLineGap=self._MAXLINEGAP)
 
-    # right_line_slope,right_line_intercept = best_fit(np.asarray(x_array_right),np.asarray(y_array_right))
-    # left_line_slope,left_line_intercept = best_fit(np.asarray(x_array_left),np.asarray(y_array_left))
+        if linesP is not None:
+            for line in linesP:
+                for x1, y1, x2, y2 in line:
+                    slope = (y2 - y1) / (x2 - x1)
+                    p1 = Point()
+                    p2 = Point()
+                    p1.x = x1
+                    p1.y = y1
+                    p1.z = 0
+                    point_arr.extend(p1)
+                    p2.x = x2
+                    p2.y = y2
+                    p2.z = 0
+                    point_arr.extend(p2)
+                    # <-- Calculating the slope.
+                    if math.fabs(slope) < .5:
+                        # <-- Only consider extreme slope
+                        continue
+                    if x1 < cv_image.shape[1]/2 and x2 < cv_image.shape[1]/2:
+                        # <-- If the slope is negative, left group
+                        cv2.circle(cv_image, (x1, y1), (5), (0, 0, 255), 3)
+                        cv2.circle(cv_image, (x2, y2), (5), (0, 0, 255), 3)
+                    else:  # <-- Otherwise, right group.
+                        cv2.circle(cv_image, (x1, y1), (5), (0, 255, 0), 3)
+                        cv2.circle(cv_image, (x2, y2), (5), (0, 255, 0), 3)
+        return cv_image
 
-    toc = time.time()
-    delta_t = toc - tic
-    print("FPS: {}".format(1/delta_t))
 
-    key = cv2.waitKey(25)
-    if key == 27:
-        break
+def main(args):
+    rospy.init_node('LinesNode')
+    node_ = PubSubNode()
+    r = rospy.get_param("rate")
+    rate = rospy.Rate(r)
+    try:
+        while not rospy.is_shutdown():
+            node_.run_detects()
+            rate.sleep()
 
-video.release()
-cv2.destroyAllWindows
+    except KeyboardInterrupt as e:
+        rospy.logfatal("shutting down ROS Lines detector Module. Error: {}".format(e))
+
+
+if __name__ == '__main__':
+    main(sys.argv)
