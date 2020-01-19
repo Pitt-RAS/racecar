f94f4423069c5f6b505ced148644493920aaea3a
Magellan <ssraible@gmail.com>
2020-01-19 17:50:17 -0500

    new git params

--- src/magellan_motion/scripts/fake_obstacles.py
+++ src/magellan_motion/scripts/fake_obstacles.py
@@ -2,8 +2,6 @@
 
 import rospy
 import numpy as np
-import sys
-import random
 
 from nav_msgs.msg import OccupancyGrid
 
@@ -11,56 +9,24 @@ if __name__ == '__main__':
     rospy.init_node('fake_obstacle')
 
     map_pub = rospy.Publisher('/grid', OccupancyGrid, queue_size=10)
-    rate_ = rospy.Rate(5)
+    rate_ = rospy.Rate(50)
 
-    resolution = 1.0
-    x_meters =1
-    y_meters=1
+    x_meters = 10
+    y_meters = 10
 
-    print(x_meters)
+    resolution = .01  # 10 cm
 
-    if rospy.has_param('~x_meters'):
-    	x_meters = rospy.get_param('/x_meters')
-    else:
-    	print("x_meters does not exist")
+    numX = int(x_meters/resolution)
+    numY = int(y_meters/resolution)
 
-    if rospy.has_param('y_meters'):
-    	y_meters = rospy.get_param('y_meters')
-    else:
-    	print("y_meters does not exist")
+    x_bounds_lower = rospy.get_param("~obst_start_x")/resolution
+    x_bounds_upper = rospy.get_param("~obst_end_x")/resolution
 
-    if rospy.has_param('~resolution'):
-    	resolution = rospy.get_param('~resolution')
-    else:
-    	print("resolution does not exist")
-
-    numX=int(x_meters/resolution)
-    numY=int(y_meters/resolution)
-
-    x_bounds_lower = 600
-    x_bounds_upper = 700
-
-    y_bounds_free = 400
-    y_bounds_free_upper = 500
+    y_bounds_free = rospy.get_param("~obst_start_y")/resolution
+    y_bounds_free_upper = rospy.get_param("~obst_end_y")/resolution
 
     map_im = np.zeros(numX*numY, dtype=int)
 
-    if '-s' in sys.argv:
-        print("straighaway")
-
-    if '-m' in sys.argv:
-        right = random.getrandbits(1)
-        if right == 1:  # right line is missing
-            print("right")
-        else:  # left line is missing
-            print("left")
-
-        print("missing line")
-
-    if '-c' in sys.argv:
-        print("curve")
-
-    # fake obstacles centered at (x/2,y/2)
     for y in range(0, numY):
         for x in range(0, numX):
             index = numY * y + x
@@ -72,13 +38,11 @@ if __name__ == '__main__':
     map_ = map_im.tolist()
 
     msg_ = OccupancyGrid()
-    msg_.header.frame_id = 'base_link'
+    msg_.header.frame_id = 'map'
     msg_.data = map_
     msg_.info.height = numY
     msg_.info.width = numX
     msg_.info.resolution = resolution
-    msg_.info.origin.position.x = -5
-    msg_.info.origin.position.y = -5
 
     while not rospy.is_shutdown():
         msg_.header.stamp = rospy.Time.now()
